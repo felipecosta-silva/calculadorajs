c133c59e338979654cc78cee09ce7a2be03005f5
Felipe Costa <felipecostati@gmail.com>
2020-01-24 23:00:42 -0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,17 @@
+// Criando uma variável para capturar o elemento
+// do HTML pelo Id. Este elemento mostrará os valores no display.
+let inputResultado = document.getElementById("inputDisplayResultado");
+
+let calculo = {
+    primeiroValor: 0,
+    segundoValor: 0,
+    funcaoParaCalcular: null
+};
+
+Window.addEventListener("load", funcation(){
+    atribuirEventos();
+})
+
+function atribuirEventos(){
+    do
+}
